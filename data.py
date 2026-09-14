a88feb45bd13827e306df4ed2697c9bf17884134
acongkentong-cloud <acongkentong@gmail.com>
2026-09-14 18:51:12 +0700

    Create data.py

---
+++ data.py
@@ -0,0 +1,11 @@
+# Data Kredensial Login Bot
+VALID_USER_ID = "WSO"
+VALID_PASSWORD = "Gacor1122"
+
+# List ID Group Telegram Target Promosi
+# (Dapatkan ID Group dengan meneruskan pesan dari group ke @userinfobot atau @RawDataBot)
+GROUPS = [
+    -1001234567890,
+    -1009876543210,
+    # Tambahkan ID group lainnya di sini...
+]
